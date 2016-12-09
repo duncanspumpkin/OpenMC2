c2ab0619bc2ab32a2e4563b2a3ae0d516beed8a5
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2016-12-09 13:13:07 +0000

    More implementation

--- src/DX9pipe.cpp
+++ src/DX9pipe.cpp
@@ -21,9 +21,27 @@
 #include "Game.hpp"
 #include <d3d9.h>
 
+struct resolution {
+    uint32_t width;
+    uint32_t height;
+    uint32_t bits;
+    uint32_t unkC;
+};
+
+struct unk_85A99C {
+    uint8_t pad000[0x57C];
+    uint32_t num_resoluitions;
+    resolution *resolutions; //0x580
+};
+
 #define glo_D3D (MC2_GLOBAL<IDirect3D9 *>(0x00858368))
 #define glo_85A1D0 (MC2_GLOBAL<uint32_t>(0x0085A1D0))
+// Some sort of struct size 0x584
+#define glo_85A99C (MC2_GLOBAL<unk_85A99C*>(0x0085A99C))
+// Compatible resolution?
+#define glo_858714 (MC2_GLOBAL<uint32_t>(0x00858714))
 
+#define loc_8586D0 (MC2_POINTER<uint32_t>(0x008586D0))
 // Likely some sort of struct
 #define loc_858728 (MC2_POINTER<uint32_t>(0x00858728))
 // Array end
@@ -32,6 +50,20 @@
 #define sub_5F1140 (MC2_PROC_PTR<void>(0x005F1140))
 // Register window and create
 #define sub_5ED560 (MC2_PROC_PTR<void, char *>(0x005ED560))
+
+// mc2: 0x005F02B0
+uint32_t sub_5F02B0(uint32_t unk1) {
+    return glo_85A99C[unk1].num_resoluitions;
+}
+
+// mc2: 0x005F02E0
+void sub_5F02E0(uint32_t unk1, uint32_t unk2, uint32_t *width, uint32_t *height, uint32_t *bits) {
+    unk_85A99C *unk = &glo_85A99C[unk1];
+
+    *width = unk->resolutions[unk2].width;
+    *height = unk->resolutions[unk2].height;
+    *bits = unk->resolutions[unk2].bits;
+}
 
 bool init_directx() {
     IDirect3D9 *pD3D = Direct3DCreate9(D3D9b_SDK_VERSION);
@@ -67,6 +99,25 @@ bool init_directx() {
         }
     }
 
-    uint32_t ebp = glo_674FB4;
-    //5f139c
+    uint32_t num_resolutions = sub_5F02B0(0);
+    if (num_resolutions > 0) {
+        for (int i = 0; i < num_resolutions; ++i) {
+            uint32_t width, height, bits;
+            sub_5F02E0(0, i, &width, &height, &bits);
+
+            if (width != glo_674FAC)
+                continue;
+
+            if (height != glo_674FB0)
+                continue;
+
+            if (bits != glo_674FB4)
+                continue;
+
+            glo_858714 = i;
+        }
+    }
+
+    memset(loc_8586D0, 0, 14 * 4);
+    //5f13ff
 }
